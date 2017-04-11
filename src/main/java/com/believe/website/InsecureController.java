f2a9d280517ca0b9fccc319ef6aa6cd8315323bb
github.com <lixingping233@163.com>
2017-04-11 21:08:27 +0800

    update

--- src/main/java/com/believe/website/InsecureController.java
+++ src/main/java/com/believe/website/InsecureController.java
@@ -1,11 +1,11 @@
 package com.believe.website;
 
+import com.believe.core.service.impl.WechatSupport;
 import com.believe.wechat.model.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p> The describe </p>
@@ -17,24 +17,20 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class InsecureController {
 
+  @Autowired
+  private WechatSupport wechatSupport;
+
   /* 我也要宣言 */
   @RequestMapping(value = "/introduce")
-  public String introduce() {
+  public String introduce(Model model) {
+    Config config = wechatSupport.jsConfig("introduce");
+    model.addAttribute("config", config);
     return "introduce";
   }
 
   @RequestMapping(value = "/general")
   public String error() {
     return "general";
-  }
-
-  @RequestMapping(value = "/config")
-  public @ResponseBody Config getJsSdksConfig(HttpServletRequest request) {
-    request.getRequestURL();
-    System.out.println(request.getRequestURI());
-    System.out.println(request.getRequestURI());
-//    return wechatSupport.jsConfig()
-    return null;
   }
 
 }
